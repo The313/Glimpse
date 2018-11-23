0d22ef9b59e6ca5cd8a4b186fde2a9323663026c
The313 <44632632+The313@users.noreply.github.com>
2018-11-23 21:30:06 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chias.a50001_1d_project_crowdchecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
