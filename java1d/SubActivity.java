dfcc434abca8e6facb9bdb6a6d3d67a5d462ae43
The313 <44632632+The313@users.noreply.github.com>
2018-12-05 04:13:08 +0800

    Update SubActivity.java

--- java1d/SubActivity.java
+++ java1d/SubActivity.java
@@ -1,36 +1,45 @@
 package com.example.chias.crowdcheck;
 
 import android.content.Intent;
-
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.view.View;
-import android.widget.Button;
-
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity{
 
-    Button Home;
-    Button Favourite;
-    public final String MAXIMUM_CAPACITY = ""; //firebase data + "/" + max capacity
-    public final String LAST_UPDATE = "Last updated on: ";// + Firebase updated time
+    ImageButton Home;
+    ImageButton Favourite;
+    int firebasecapacity = 0;
+    int maxcapacity = 100;   //TODO: SET MAX CAPACITY BASED ON LOCATION
+    int maxcapacitywidth;
+    public final String LAST_UPDATE = "Last updated on: " + "" + " (" + firebasecapacity + "/" + maxcapacity + ")";//TODO: + Firebase updated time
     private SwipeRefreshLayout swipeCont;
+    TextView LastUpdate;
+    ImageView progressback;
+    ImageView progressfront;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details1);
 
+        LastUpdate = findViewById(R.id.textView14);
+
         swipeCont = (SwipeRefreshLayout) findViewById(R.id.swiperlayout);
         swipeCont.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                //refresh firebase capacity, change MAXIMUM_CAPACITY
-                //replace last update string with LAST_UPDATE
+
+                //TODO: refresh firebase capacity
+                progressback.setMaxWidth(firebasecapacity/maxcapacity * maxcapacitywidth);
+                LastUpdate.setText(LAST_UPDATE);
             }
         });
         swipeCont.setColorSchemeResources(android.R.color.holo_blue_bright,
@@ -39,7 +48,16 @@ public class SubActivity extends AppCompatActivity{
                 android.R.color.holo_red_light);
 
 
-        Home = findViewById(R.id.imageButton3);
+
+        progressfront = findViewById(R.id.progressfront);
+        maxcapacitywidth = progressfront.getMaxWidth();
+        //read data from firebase
+        progressback = findViewById(R.id.progressback);
+        progressback.setMaxWidth(firebasecapacity/maxcapacity * maxcapacitywidth);
+
+
+
+        Home = findViewById(R.id.home);
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,7 +65,7 @@ public class SubActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-        Favourite = findViewById(R.id.imageButton4);
+        Favourite = findViewById(R.id.favourite);
         Favourite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,4 +79,3 @@ public class SubActivity extends AppCompatActivity{
         });
     }
 }
-
